c213ef67cba99f01ace113325c50f1bee88fc41d
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-08-24 12:36:03 +0100

    Initial commit

---
+++ CsvRemoveNullRow/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Classes.Enums;
+					
+public class Program
+{
+	public static void Main()
+	{
+		/*
+			Problem: Take a csv string and remove any row that contains
+			a NULL value.
+
+			Csv strings are fed in through an enum in the classes folder.
+		*/
+
+		// Call Solution
+		foreach (CsvStringsEnum examples in Enum.GetValues(typeof(CsvStringsEnum)))
+    	{
+			Console.WriteLine(Solution(examples.ToDescriptionString()));
+    	}
+	}
+	#region SOLUTION
+	static string Solution(string S)
+	{
+		// Split String Into List (Items In List Are Treated As Rows);
+		List<string> myList = new List<string>(S.Split('\n'));
+		// Find And Remove Row Which Contains NULL Value;
+		myList = RemoveNullRow(myList);
+		// Re-Connect String And Return For Console Print
+		return ConvertToString(myList);
+	}
+	static List<string> RemoveNullRow(List<string> tableList)
+	{
+		tableList.RemoveAll(u => u.Contains("NULL"));
+		return tableList;
+	}
+	static string ConvertToString(List<string> tableList)
+	{
+		string newString = "";
+		tableList.ForEach(item => newString += item + "\n");
+		return newString;
+	}
+	#endregion
+}
---
+++ CsvRemoveNullRow/classes/enums.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+
+namespace Classes.Enums 
+{
+    public enum CsvStringsEnum
+    {
+        [Description("id,name,age,score\n1,Jack,NULL,12\n17,Betty,28,11")]
+        Example1 = 1,
+        [Description("id,name,age,score\n1,Jack,22,12\n17,Betty,28,11")]
+        Example2 = 2,
+        [Description("id,name,age,score\n1,Jack,22,12\n17,NULL,28,11")]
+        Example3 = 3
+    }
+
+    public static class CsvStringsExtensions
+    {
+        public static string ToDescriptionString(this CsvStringsEnum val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+            .GetType()
+            .GetField(val.ToString())
+            .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    } 
+}
