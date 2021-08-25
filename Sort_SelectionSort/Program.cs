dbad29efc7af19288d1d62489c3438531a59db5a
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-08-25 14:42:31 +0100

    Added Selection Sort

---
+++ Sort_SelectionSort/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Classes.Data;
+using Classes.LargeData;
+using Classes.StopWatch;
+
+namespace Sort_SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+			    Problem: Sort through array of int using Selection Sort Algorithm.
+
+			    int arrays are fed in through a static class in the classes folder.
+		    */
+
+            // Call Solution
+            foreach (int[] intArray in Data.UnsortedData)
+            {
+                Console.WriteLine(Solution(intArray));
+            }
+        }
+
+        #region SOLUTION
+        static string Solution(int[] dataToSort)
+	    {
+            // Print Unsorted Array
+            Console.WriteLine("\nUnsorted: " + ArrayToString(dataToSort));
+            // Sort Array
+            int[] sortedArray = SelectionSort(dataToSort);
+            // Return Text
+            return "Sorted: " + ArrayToString(sortedArray) + "\n------------------";
+	    }
+        static int[] SelectionSort(int[] intArray)
+        {
+            StopWatchHandler stopwatch = new StopWatchHandler();
+            Console.WriteLine("Timer Started => Sorting");
+            stopwatch.StartStopWatch();
+            
+            for (int i = 0; i < intArray.Length-1; i++)
+            {
+                int minimum = i;
+                for(int j = i+1; j < intArray.Length; j++)
+                    if(intArray[j] < intArray[minimum]) 
+                        minimum = j;
+            
+                int temp = intArray[i]; 
+                intArray[i] = intArray[minimum];
+                intArray[minimum] = temp;
+            }       
+
+            stopwatch.StopStopWatch();
+            Console.WriteLine(stopwatch.GetTime());
+            return intArray;       
+        }
+        static string ArrayToString(int[] intArray)
+        {
+            string returnString = "";
+            for (int i=0; i < 3; i++) 
+                if(i != 2)
+                    returnString += intArray[i] + ", ";
+                else
+                    returnString += intArray[i] + ". . . ";
+
+            return returnString;
+        }
+        #endregion
+    }
+}
